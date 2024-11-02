18ce15f32c03d3b063901d957eef004a33730b24
Marcos Gonçalves <mag.junior@hotmail.com>
2024-11-02 10:29:16 -0300

    Fix: Cálculo de ferimento, atributos e vida

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -18,8 +18,8 @@ export class CronicasActor extends Actor {
    * Calculate Character attributes according to his actual points and penalities
    */
   _evaluateAttributes() {
-    for (let [key, atributo] of Object.entries(data.atributos)) {
-      atributo.total = atributo.valor + atributo.outros - data.penalidades.ferimento;
+    for (let [key, atributo] of Object.entries(this.system.atributos)) {
+      atributo.total = atributo.valor + atributo.outros - this.system.penalidades.ferimento;
     }
   }
 
@@ -27,30 +27,28 @@ export class CronicasActor extends Actor {
    * Prepare Character health bar and suffering status
    */
   _evaluateHealth() {
-    const parrudice = data.atributos.parrudice.valor;
+    const parrudice = this.system.atributos.parrudice.valor;
 
-    if (data.attributes.vigor.max != (1 + parrudice)) {
-      data.attributes.vigor.value -= data.attributes.vigor.max - (1 + parrudice);
+    if (this.system.attributes.vigor.max != (1 + parrudice)) {
+      this.system.attributes.vigor.value -= this.system.attributes.vigor.max - (1 + parrudice);
     }
-    data.attributes.vigor.max = 1 + parrudice;
+    this.system.attributes.vigor.max = 1 + parrudice;
 
-    if (data.attributes.vigor.value > data.attributes.vigor.max) {
-      data.attributes.vigor.value = data.attributes.vigor.max;
-    }
-
-    if (data.penalidades.ferimento < 0) {
-      data.penalidades.ferimento = 0;
+    if (this.system.attributes.vigor.value > this.system.attributes.vigor.max) {
+      this.system.attributes.vigor.value = this.system.attributes.vigor.max;
     }
 
-    if (data.penalidades.ferimento == data.penalidades.danoRecebido) {
-      data.penalidades.ferimento = data.attributes.vigor.max - data.attributes.vigor.value;
-      data.penalidades.danoRecebido = data.attributes.vigor.max - data.attributes.vigor.value;
+    if (this.system.penalidades.ferimento < 0) {
+      this.system.penalidades.ferimento = 0;
+    }
+
+    if (this.system.penalidades.ferimento == this.system.penalidades.danoRecebido) {
+      this.system.penalidades.ferimento = this.system.attributes.vigor.max - this.system.attributes.vigor.value;
+      this.system.penalidades.danoRecebido = this.system.attributes.vigor.max - this.system.attributes.vigor.value;
     } else {
-      data.attributes.vigor.value = data.attributes.vigor.max - data.penalidades.ferimento;
-      data.penalidades.danoRecebido = data.attributes.vigor.max - data.attributes.vigor.value;
-      data.penalidades.ferimento = data.attributes.vigor.max - data.attributes.vigor.value;
+      this.system.attributes.vigor.value = this.system.attributes.vigor.max - this.system.penalidades.ferimento;
+      this.system.penalidades.danoRecebido = this.system.attributes.vigor.max - this.system.attributes.vigor.value;
+      this.system.penalidades.ferimento = this.system.attributes.vigor.max - this.system.attributes.vigor.value;
     }
-
-    this.data.update({ data: data })
   }
 }
